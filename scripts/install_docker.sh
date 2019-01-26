7b19dcb7a4f20df7d3e2f99c30bd18ca0e470760
Daniel Fridkin <danielfridkin@gmail.com>
2019-01-26 17:31:00 -0500

    noq

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -1,14 +1,14 @@
-apt-get -qq -o Dpkg::Use-Pty=0 update
-apt-get -qq -o Dpkg::Use-Pty=0 install -y \
+apt-get update
+apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg2 \
     software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - -qq
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - 
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
-   stable" -qq
-apt-get -qq -o Dpkg::Use-Pty=0 update
-apt-get -qq -o Dpkg::Use-Pty=0 install -y docker-ce
+   stable"
+apt-get update
+apt-get install -y docker-ce
