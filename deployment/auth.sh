5b713a17f6cf0c3eb74528232131adbaae51d2bf
Daniel Fridkin <danielfridkin@gmail.com>
2019-01-26 17:00:46 -0500

    cmon now

--- deployment/auth.sh
+++ deployment/auth.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/sh
-
 echo $GCLOUD_SERVICE_KEY | gcloud auth activate-service-account --key-file=-
 gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
 gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
