4d4bda5b9d59ca87a8bc0969dde853d2ad57d3a8
Daniel Fridkin <danielfridkin@gmail.com>
2019-01-26 17:14:14 -0500

    iadjfoijfoiajasdf

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -5,7 +5,7 @@ apt-get install -y \
     curl \
     gnupg2 \
     software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
