9139b896f8131e24c8f7ded64b800e0c31966755
Daniel Fridkin <danielfridkin@gmail.com>
2019-01-27 16:53:07 -0500

    One last extraneous commit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@ if [ "$CIRCLE_BRANCH" == "master" ]
 then
     export IMAGE_TAG="latest"
 else
-    export IMAGE_TAG=$CIRCLE_SHA1
+    export IMAGE_TAG=$CIRCLE_BRANCH
 fi
 
 docker push us.gcr.io/${GOOGLE_PROJECT_ID}/${CIRCLE_PROJECT_REPONAME}:$IMAGE_TAG
