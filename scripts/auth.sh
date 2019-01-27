220539c4b2479a07982f7951732f871e45452b7f
Daniel Fridkin <danielfridkin@gmail.com>
2019-01-27 16:48:03 -0500

    removed deprecated gcloud docker call

--- scripts/auth.sh
+++ scripts/auth.sh
@@ -1,3 +1,8 @@
+# Authenticate to gcloud
+
 echo $GCLOUD_SERVICE_KEY | gcloud auth activate-service-account --key-file=-
 gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
 gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
+
+# Configure docker to use gcloud credentials
+gcloud auth configure-docker
