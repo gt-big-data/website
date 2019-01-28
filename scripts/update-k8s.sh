20129bc8201f6bd03eeaf1f3bce9e7db6e9726e4
Daniel Fridkin <danielfridkin@gmail.com>
2019-01-27 19:41:47 -0500

    will this work

--- scripts/update-k8s.sh
+++ scripts/update-k8s.sh
@@ -1,9 +1,11 @@
 if git diff --name-only HEAD~1 HEAD | grep -q k8s.yml
 then
     echo "Detected some changes to k8s.yml, updating configuration..."
-    apt-get -qq update
     apt-get -qqy install gettext-base
     envsubst < ${HOME}/project/k8s.yml > ${HOME}/patched_k8s.yml
+
+    gcloud --quiet container clusters get-credentials ${GOOGLE_PROJECT_ID}
+    
     kubectl apply -f ${HOME}/patched_k8s.yml
     kubectl rollout status deployment/${PROJECT_NAME}
 else
