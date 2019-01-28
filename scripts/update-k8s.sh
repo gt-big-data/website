7845be9249d10ed634f3d4d525c8797e231e7d72
Daniel Fridkin <danielfridkin@gmail.com>
2019-01-28 16:04:08 -0500

    cmon ci

--- scripts/update-k8s.sh
+++ scripts/update-k8s.sh
@@ -3,11 +3,11 @@ set -e
 # Only update if something k8s-related diff's
 if git diff --name-only HEAD~1 HEAD | grep -q k8s
 then
-    echo "Detected some changes to k8s configuration, updating..."
+    echo "Detected some changes to k8s config, updating..."
     gcloud --quiet container clusters get-credentials ${GOOGLE_PROJECT_ID} --region=${GOOGLE_COMPUTE_REGION}
 
     kubectl apply -f --recursive=true ./k8s
     kubectl rollout status deployment/${CIRCLE_PROJECT_REPONAME}
 else
-    echo "No changes detected in k8s.yml, no update needed"
+    echo "No changes detected in k8s config, no update needed"
 fi
