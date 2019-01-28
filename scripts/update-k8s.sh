8d491b01e5366d76cca3dd53c28170d26f83f3cf
Daniel Fridkin <danielfridkin@gmail.com>
2019-01-28 16:10:21 -0500

    I've really had it this time

--- scripts/update-k8s.sh
+++ scripts/update-k8s.sh
@@ -5,7 +5,7 @@ then
     echo "Detected changes to k8s config, updating..."
     gcloud --quiet container clusters get-credentials ${GOOGLE_PROJECT_ID} --region=${GOOGLE_COMPUTE_REGION}
 
-    kubectl apply -f --recursive=true ./k8s
+    kubectl apply -f ./k8s --recursive=true
     kubectl rollout status deployment/${CIRCLE_PROJECT_REPONAME}
 else
     echo "No changes detected in k8s config, no update needed."
