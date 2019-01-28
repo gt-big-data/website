6cc7c54dcc82ae1a8e6e0ca85dceec581de02746
Daniel Fridkin <danielfridkin@gmail.com>
2019-01-27 19:32:04 -0500

    k8s pls deploy

--- scripts/update-k8s.sh
+++ scripts/update-k8s.sh
@@ -1,6 +1,8 @@
 if git diff --name-only HEAD~1 HEAD | grep -q k8s.yml
 then
     echo "Detected some changes to k8s.yml, updating configuration..."
+    apt-get -qq update
+    apt-get -qqy install gettext-base
     envsubst < ${HOME}/project/k8s.yml > ${HOME}/patched_k8s.yml
     kubectl apply -f ${HOME}/patched_k8s.yml
     kubectl rollout status deployment/${PROJECT_NAME}
