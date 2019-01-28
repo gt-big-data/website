0c1731dec030aff1a8da260da7855de73e4a8904
Daniel Fridkin <danielfridkin@gmail.com>
2019-01-27 21:15:18 -0500

    restructured k8s ymls

--- scripts/update-k8s.sh
+++ scripts/update-k8s.sh
@@ -1,12 +1,9 @@
-if git diff --name-only HEAD~1 HEAD | grep -q k8s.yml
+if git diff --name-only HEAD~1 HEAD | grep -q k8s
 then
-    echo "Detected some changes to k8s.yml, updating configuration..."
-    apt-get -qqy install gettext-base
-    envsubst < ${HOME}/project/k8s.yml > ${HOME}/patched_k8s.yml
-
+    echo "Detected some changes to k8s configuration, updating..."
     gcloud --quiet container clusters get-credentials ${GOOGLE_PROJECT_ID} --region=${GOOGLE_COMPUTE_REGION}
 
-    kubectl apply -f ${HOME}/patched_k8s.yml
+    kubectl apply -Rf ${HOME}/k8s/*.yml
     kubectl rollout status deployment/${CIRCLE_PROJECT_REPONAME}
 else
     echo "No changes detected in k8s.yml, no update needed"
