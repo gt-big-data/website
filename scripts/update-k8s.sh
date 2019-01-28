f4e4a5ead57bf80f361dcaba137c5840a42b74c8
Daniel Fridkin <danielfridkin@gmail.com>
2019-01-27 19:52:56 -0500

    aodijfoisjf

--- scripts/update-k8s.sh
+++ scripts/update-k8s.sh
@@ -7,7 +7,7 @@ then
     gcloud --quiet container clusters get-credentials ${GOOGLE_PROJECT_ID} --region=${GOOGLE_COMPUTE_REGION}
 
     kubectl apply -f ${HOME}/patched_k8s.yml
-    kubectl rollout status deployment/${PROJECT_NAME}
+    kubectl rollout status deployment/${CIRCLE_PROJECT_REPONAME}
 else
     echo "No changes detected in k8s.yml, no update needed"
 fi
