1a836f1a4105514eeae8fa9a506eccf1149272ee
Daniel Fridkin <danielfridkin@gmail.com>
2019-01-27 19:22:43 -0500

    lets go kubernetes

--- scripts/auth.sh
+++ scripts/auth.sh
@@ -3,6 +3,3 @@
 echo $GCLOUD_SERVICE_KEY | gcloud auth activate-service-account --key-file=-
 gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
 gcloud --quiet config set compute/zone ${GOOGLE_COMPUTE_ZONE}
-
-# Configure docker to use gcloud credentials
-gcloud auth configure-docker
---
+++ scripts/update-k8s.sh
@@ -0,0 +1,9 @@
+if [ git diff --name-only HEAD~1 HEAD | grep k8s.yml ]
+then
+    echo "Detected some changes to k8s.yml, updating configuration..."
+    envsubst < ${HOME}/project/k8s.yml > ${HOME}/patched_k8s.yml
+    kubectl apply -f ${HOME}/patched_k8s.yml
+    kubectl rollout status deployment/${PROJECT_NAME}
+else
+    echo "No changes detected in k8s.yml, no update needed"
+fi
