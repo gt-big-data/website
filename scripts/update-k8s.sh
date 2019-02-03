1b18bf5565343ab0c42b370deed2277df67a159b
Daniel Fridkin <danielfridkin@gmail.com>
2019-02-03 14:47:57 -0500

    oh lord jesus im dying

--- scripts/update-k8s.sh
+++ scripts/update-k8s.sh
@@ -6,7 +6,7 @@ then
     gcloud --quiet container clusters get-credentials ${GOOGLE_PROJECT_ID} --region=${GOOGLE_COMPUTE_REGION}
 
     # Remove the validate=false flag when GKE updates to 1.13
-    kubectl create --validate=false -f ./k8s --recursive=true --dry-run=true -o yaml | kubectl apply --validate=false -f -
+    kubectl apply --validate=false -f ./k8s --recursive=true
     kubectl rollout status deployment/${CIRCLE_PROJECT_REPONAME}
 else
     echo "No changes detected in k8s config, no update needed."
