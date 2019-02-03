a33064a721cf2fbf46a829f31b40aa0010d2ffdb
Daniel Fridkin <danielfridkin@gmail.com>
2019-02-03 14:20:00 -0500

    pls cert-manager

--- scripts/update-k8s.sh
+++ scripts/update-k8s.sh
@@ -5,7 +5,7 @@ then
     echo "Detected changes to k8s config, updating..."
     gcloud --quiet container clusters get-credentials ${GOOGLE_PROJECT_ID} --region=${GOOGLE_COMPUTE_REGION}
 
-    kubectl apply -f ./k8s --recursive=true
+    kubectl create -f ./k8s --recursive=true --dry-run=true -o yaml | kubectl apply --validate=false -f -
     kubectl rollout status deployment/${CIRCLE_PROJECT_REPONAME}
 else
     echo "No changes detected in k8s config, no update needed."
