2ac4b4d2c5d8561722fbf76e051ce1ce6591e1fe
Daniel Fridkin <danielfridkin@gmail.com>
2019-02-04 14:43:58 -0500

    Fixed testing

--- src/app.py
+++ src/app.py
@@ -6,7 +6,3 @@ app._static_folder = "./static"
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, debug=False)
